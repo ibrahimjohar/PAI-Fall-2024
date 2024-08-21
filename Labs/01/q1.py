6d0cac4e7648f62fb04d85361f3e51a293686ef4
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-08-21 09:42:55 +0500

    Create q1.py

---
+++ Labs/01/q1.py
@@ -0,0 +1,11 @@
+#name: ibrahim johar farooqi
+#date: 21 august 2024
+#lab: 01
+#task: 01
+
+weight = float(input("Enter weight(kg): "))
+height = float(input("Enter height:"))
+
+bmi = (weight) / (height*height)
+
+print ("BMI is: ", bmi)
