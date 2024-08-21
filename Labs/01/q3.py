d22051e545724dc4341425c280c20ba879f87a58
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-08-21 09:53:04 +0500

    Create q3.py

---
+++ Labs/01/q3.py
@@ -0,0 +1,16 @@
+#name: ibrahim johar farooqi
+#date: 21 august 2024
+#lab: 01
+#task: 03
+
+array=[]
+even_count=0
+
+for x in range(0,5):
+    user_input = int (input("Enter Number: "))
+    array.append(user_input)
+    if user_input % 2 == 0:
+        even_count += 1
+
+
+print ("Even Count: ", even_count)
