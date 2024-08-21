3a7242b80e1ed173a61149c2ad873a2d3758383b
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-08-21 10:21:02 +0500

    Update q4.py

--- Labs/01/q4.py
+++ Labs/01/q4.py
@@ -3,12 +3,12 @@
 #lab: 01
 #task: 04
 
-array=[]
+a1=[]
 sum = 0
 
 for x in range(0,5):
     user_input = int(input("Enter Number: "))
-    array.append(user_input)
+    a1.append(user_input)
     sum += user_input
     
 
