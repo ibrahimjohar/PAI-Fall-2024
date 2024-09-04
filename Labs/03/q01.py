3e059600419850ac38a3f664e5a7ae458038dfb0
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-09-04 08:59:30 +0500

    Update q01.py

--- Labs/03/q01.py
+++ Labs/03/q01.py
@@ -3,19 +3,23 @@
 #lab: 03
 #task: 1
 
-try:
-    with open('task1text.txt', 'r') as fileObj:
-        content = fileObj.read()
+def FileWordCharReadingProgram(fileName):
+    try:
+        with open(fileName, 'r') as fileObj:
+            content = fileObj.read()
 
-    char_count = len(content)
-    print(f"character count: {char_count}")
+        char_count = len(content)
+        print(f"character count: {char_count}")
 
-    word_count = len(content.split())
-    print(f"word count: {word_count}")
+        word_count = len(content.split())
+        print(f"word count: {word_count}")
 
-except FileNotFoundError:
-    print("the file does not exist/not found.")
-except IOError:
-    print("an error occured during reading the file")
-except Exception as e:
-    print(f"unexpected error occured: {e}")
+    except FileNotFoundError:
+        print("the file does not exist/not found.")
+    except IOError:
+        print("an error occured during reading the file")
+    except Exception as e:
+        print(f"unexpected error occured: {e}")
+
+fileName = 'task1text.txt'
+FileWordCharReadingProgram(fileName)
