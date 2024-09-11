d87f34c43b50c19e28a4efe08925fdce8d8c6730
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-09-11 09:54:29 +0500

    Create q01.py

---
+++ Labs/04/q01.py
@@ -0,0 +1,14 @@
+#name: ibrahim johar farooqi
+#date: 11 september 2024
+#lab: 04
+#task: 1
+
+a = ["He", "th", "i", "sal"]
+b = ["llo", "is", "s", "man"]
+
+result = []
+
+for x, y in zip(a,b):
+    result.append(x+y)
+
+print(result)
