94f3ad99fb6848c39a31854b7f8a54f43e539ba1
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-09-25 10:14:03 +0500

    Create q01.py

---
+++ Labs/06/q01.py
@@ -0,0 +1,10 @@
+#name: ibrahim johar farooqi
+#date: 25 september 2024
+#lab: 06
+#task: 1
+
+import pandas as pd
+
+data = pd.read_csv("movie_dataset.csv")
+
+print(data[(data['revenue'] > 2000000) & (data['budget'] < 1000000)])
