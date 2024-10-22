11ec0e10adb81522fb429a4dfa92483465d1a3c7
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2024-10-22 23:55:26 +0500

    Update q04.py

--- Labs/08/q04.py
+++ Labs/08/q04.py
@@ -1,7 +1,12 @@
+#name: ibrahim johar farooqi
+#date: 10 october 2024
+#lab: 07
+#task: 04
+
 import numpy as np
 
 datatype = [('name', 'S15'), ('class', int), ('height', float)]
-student_det = [('James', 5, 48.5), ('Nail', 6, 52.5), ('Paul', 5, 42.10), ('Pit', 5, 40.11)]
+student_det = [('ibrahim', 5, 48.5), ('haris', 6, 52.5), ('joshua', 5, 42.10), ('patel', 5, 40.11)]
 
 #creating a structured array
 students = np.array(student_det, dtype = datatype)
